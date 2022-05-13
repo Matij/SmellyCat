9b8cebdb220c72240455bb15483c9fc0721e5018
Marta <matij.eng@gmail.com>
2022-05-13 23:09:56 +0100

    Project setup: first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+import buildConfig.*
+import buildConfig.Deps.addUnitTest
+import buildConfig.Deps.implementationCompose
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = appConfig.compileSdk
+
+    defaultConfig {
+        applicationId = appConfig.applicationId
+        minSdk = appConfig.minSdk
+        targetSdk = appConfig.targetSdk
+        versionCode = appConfig.versionCode
+        versionName = appConfig.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes.all {
+        buildConfigField("String", "REST_CAT_API_BASE_URL", "\"https://api.thecatapi.com/v1\"")
+        buildConfigField("String", "REST_CAT_API_KEY", "\"0d627083-3eb0-4da7-b38e-1eba46662a91\"")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
+}
+
+dependencies {
+    implementation(Deps.AndroidX.coreKtx)
+    implementation(Deps.AndroidX.appCompat)
+    implementation(Deps.Material.material)
+    implementationCompose()
+
+    // DI
+    implementation(Deps.Hilt.hiltAndroid)
+    kapt(Deps.Hilt.hiltCompiler)
+
+    // Networking
+    implementation(Deps.OkHttp.okhttp)
+    implementation(Deps.OkHttp.loggingInterceptor)
+    implementation(Deps.Retrofit.retrofit)
+    implementation(Deps.Retrofit.retrofitConverters)
+    implementation(Deps.Retrofit.converterMoshi)
+    implementation(Deps.Moshi.moshi)
+    implementation(Deps.Moshi.moshiAdapters)
+    kapt(Deps.Moshi.moshiKotlinCodegen)
+
+    // Coroutines
+    implementation(Deps.Coroutines.core)
+    implementation(Deps.Coroutines.android)
+
+    // Test
+    addUnitTest()
+}
---
+++ buildSrc/src/main/java/buildConfig/Deps.kt
@@ -0,0 +1,130 @@
+package buildConfig
+
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+const val kotlinVersion = "1.6.0"
+
+object appConfig {
+    const val applicationId = "com.martafoderaro.smellycat"
+
+    const val compileSdk = 31
+    const val buildTools = "31.0.0"
+
+    const val minSdk = 21
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object Deps {
+    object AndroidX {
+        const val activityCompose = "androidx.activity:activity-compose:1.4.0"
+        const val appCompat = "androidx.appcompat:appcompat:1.4.1"
+        const val coreKtx = "androidx.core:core-ktx:1.7.0"
+        const val pagingCompose = "androidx.paging:paging-compose:1.0.0-alpha14"
+    }
+
+    object Compose {
+        const val version = "1.1.0-beta03"
+
+        const val layout = "androidx.compose.foundation:foundation-layout:$version"
+        const val foundation = "androidx.compose.foundation:foundation:$version"
+        const val ui = "androidx.compose.ui:ui:$version"
+        const val material = "androidx.compose.material:material:$version"
+        const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+        const val runtime = "androidx.compose.runtime:runtime:$version"
+        const val tooling = "androidx.compose.ui:ui-tooling:$version"
+    }
+
+    object Coroutines {
+        private const val version = "1.6.1"
+
+        const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
+        const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+        const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+    }
+
+    object Hilt {
+        private const val version = "2.38.1"
+        const val hiltAndroid = "com.google.dagger:hilt-android:$version"
+        const val hiltCompiler = "com.google.dagger:hilt-compiler:$version"
+        const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+    }
+
+    object Kotlin {
+        const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
+        const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion"
+    }
+
+    object Lifecycle {
+        private const val version = "2.4.0"
+
+        const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version" // viewModelScope
+        const val runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:$version" // lifecycleScope
+        const val commonJava8 = "androidx.lifecycle:lifecycle-common-java8:$version"
+        const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha05"
+    }
+
+    object Material {
+        const val material = "com.google.android.material:material:1.6.0"
+    }
+
+    object Moshi {
+        private const val version = "1.13.0"
+        const val moshi = "com.squareup.moshi:moshi:$version"
+        const val moshiAdapters = "com.squareup.moshi:moshi-adapters:$version"
+        const val moshiKotlin = "com.squareup.moshi:moshi-kotlin:$version"
+        const val moshiKotlinCodegen = "com.squareup.moshi:moshi-kotlin-codegen:$version"
+    }
+
+    object OkHttp {
+        private const val version = "4.9.3"
+        const val okhttp = "com.squareup.okhttp3:okhttp:$version"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+        private const val versionConverters = "2.8.1"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val retrofitConverters = "com.squareup.retrofit2:retrofit-converters:$versionConverters"
+        const val converterMoshi = "com.squareup.retrofit2:converter-moshi:$version"
+    }
+
+    object Test {
+        const val espresso = "androidx.test.espresso:espresso-core:3.4.0"
+        const val extJUnit = "androidx.test.ext:junit:1.1.3"
+        const val jUnit = "junit:junit:4.13.2"
+
+        const val kotlinJUnit = "org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion"
+        const val mockk = "io.mockk:mockk:1.12.1"
+    }
+
+    fun DependencyHandler.implementationCompose() {
+        arrayOf(
+            AndroidX.activityCompose,
+            AndroidX.pagingCompose,
+            Lifecycle.viewModelCompose,
+            Compose.layout,
+            Compose.foundation,
+            Compose.ui,
+            Compose.material,
+            Compose.materialIconsExtended,
+            Compose.runtime,
+        ).forEach { add("implementation", it) }
+
+        add("debugImplementation", Compose.tooling)
+        add("debugImplementation", Kotlin.reflect)
+    }
+
+    fun DependencyHandler.addUnitTest(testImplementation: Boolean = true) {
+        val configName = if (testImplementation) "testImplementation" else "implementation"
+
+        add(configName, Coroutines.test)
+        add(configName, Test.espresso)
+        add(configName, Test.extJUnit)
+        add(configName, Test.jUnit)
+        add(configName, Test.kotlinJUnit)
+        add(configName, Test.mockk)
+    }
+}
