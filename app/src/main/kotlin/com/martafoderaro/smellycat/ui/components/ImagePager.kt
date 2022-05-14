0daeab88789016f4b0805ed51f67fcbdac723a74
Marta <matij.eng@gmail.com>
2022-05-14 23:49:58 +0100

    Add Image transition animation

--- app/src/main/kotlin/com/martafoderaro/smellycat/ui/components/ImagePager.kt
+++ app/src/main/kotlin/com/martafoderaro/smellycat/ui/components/ImagePager.kt
@@ -1,9 +1,6 @@
 package com.martafoderaro.smellycat.com.martafoderaro.smellycat.ui.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,17 +15,38 @@ import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
 import com.martafoderaro.smellycat.com.martafoderaro.smellycat.domain.model.BreedImage
+import kotlin.math.absoluteValue
+
+private val maxOffset = 70.dp
 
 @ExperimentalPagerApi
 @Composable
 fun ImagePager(images: List<BreedImage>, modifier: Modifier = Modifier) {
     val pagerState = rememberPagerState()
+    val currentIndex = pagerState.currentPage
+    val currentPageOffset = pagerState.currentPageOffset
     HorizontalPager(count = images.size, state = pagerState, modifier = modifier) { index ->
+        val offset = maxOffset * when (index) {
+            currentIndex -> {
+                currentPageOffset.absoluteValue
+            }
+            currentIndex - 1 -> {
+                1 + currentPageOffset.coerceAtMost(0f)
+            }
+            currentIndex + 1 -> {
+                1 - currentPageOffset.coerceAtLeast(0f)
+            }
+            else -> {
+                1f
+            }
+        }
+
         Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
             val currentImage = images[index]
             SubcomposeAsyncImage(
                 model = currentImage.url,
-                contentDescription = currentImage.id
+                contentDescription = currentImage.id,
+                modifier = Modifier.offset(y = offset)
             ) {
                 val state = painter.state
                 if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
