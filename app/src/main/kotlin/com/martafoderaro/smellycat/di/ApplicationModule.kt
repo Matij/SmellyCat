ba50891fbfdac4854fb34b9e265877c0d5f392d3
Matij <matij.eng@gmail.com>
2022-11-30 18:35:36 +0000

    Add di provide for newly introduces object

--- app/src/main/kotlin/com/martafoderaro/smellycat/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/martafoderaro/smellycat/di/ApplicationModule.kt
@@ -3,6 +3,7 @@ package com.martafoderaro.smellycat.di
 import android.app.Application
 import android.content.Context
 import com.martafoderaro.smellycat.BuildConfig
+import com.martafoderaro.smellycat.com.martafoderaro.smellycat.util.ConnectionStatusProvider
 import com.martafoderaro.smellycat.data.datasources.network.RestDebug
 import dagger.Module
 import dagger.Provides
@@ -17,4 +18,7 @@ class ApplicationModule {
     fun provideRestDebug(): Boolean = BuildConfig.BUILD_TYPE == "debug"
     @Provides
     fun provideContext(application: Application): Context = application.applicationContext
+
+    @Provides
+    fun provideConnectionStatusProvider() = ConnectionStatusProvider()
 }
