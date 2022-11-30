0ad4e75d7757d1bf038fd2ef4b956a118e3b5fda
Matij <matij.eng@gmail.com>
2022-11-30 18:29:16 +0000

    Move isConnected to ConnectionStatusProvider object

---
+++ app/src/main/kotlin/com/martafoderaro/smellycat/util/ConnectionStatusProvider.kt
@@ -0,0 +1,19 @@
+package com.martafoderaro.smellycat.com.martafoderaro.smellycat.util
+
+import timber.log.Timber
+import java.io.IOException
+
+class ConnectionStatusProvider {
+    fun isConnected(): Boolean {
+        val command = "ping -c 1 google.com"
+        var connected = false
+        try {
+            connected = Runtime.getRuntime().exec(command).waitFor() == 0
+        } catch (e: InterruptedException) {
+            Timber.d(e)
+        } catch (e: IOException) {
+            Timber.d(e)
+        }
+        return connected
+    }
+}
